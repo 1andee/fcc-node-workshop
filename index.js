6c49b23f4dea667c03fc50042b60220ad6cc5f0b
Ian Duke <ian.c.duke@gmail.com>
2017-07-29 15:03:13 -0400

    Add dependencies

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("this is the index.js file!!!111oneone!!!")
+const express = require('express')
+const app = express()
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
